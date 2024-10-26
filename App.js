f0633579394c3459010e45e3c078f8e4fe0aa75b
SangWon7242 <gotkdqja@gmail.com>
2024-10-26 13:30:45 +0900

    navigation.navigate를 이용하여 화면간 전환

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  // 복잡한 구조인 경우에만 필요하다.
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text style={{ fontSize: 40, fontWeight: "bold" }}>메인 화면</Text>
+      <Button
+        title="상세 페이지 이동"
+        onPress={() => navigation.navigate("Details")}
+      />
     </View>
   );
 };
 
-const DetailScreen = () => {
+const DetailScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text style={{ fontSize: 40, fontWeight: "bold" }}>상세보기 화면</Text>
+      <Button title="홈으로 이동" onPress={() => navigation.navigate("Home")} />
+      <Button
+        title="상세 페이지로 이동"
+        onPress={() => navigation.push("Details")}
+      />
     </View>
   );
 };
