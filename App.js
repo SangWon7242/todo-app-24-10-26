ec3e534951ceadf85d53969f9d8236f3f6267c16
SangWon7242 <gotkdqja@gmail.com>
2024-10-27 13:15:32 +0900

    tabConfig와 각각의 screen을 별도의 파일로 분리

--- App.js
+++ App.js
@@ -1,143 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  TextInput,
-  Pressable,
-} from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React, { useState } from "react";
-import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 
-const HomeScreen = () => {
-  const navigation = useNavigation();
-  // 복잡한 구조인 경우에만 필요하다.
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 40, fontWeight: "bold" }}>메인 화면</Text>
-    </View>
-  );
-};
-
-const TodoWriteScreen = ({ navigation }) => {
-  const [todo, setTodo] = useState("");
-
-  return (
-    <>
-      <TextInput
-        multiline
-        onChangeText={setTodo}
-        value={todo}
-        placeholder="할 일을 작성해주세요."
-        style={{
-          flex: 0.3,
-          padding: 10,
-          backgroundColor: "#fff",
-          borderRadius: 10,
-          borderWidth: 2,
-          margin: 10,
-        }}
-      />
-      <Pressable
-        onPress={() => {
-          navigation.navigate("Details", { todo });
-          setTodo("");
-        }}
-      >
-        <Text
-          style={{
-            padding: 10,
-            backgroundColor: "#fff",
-            borderRadius: 10,
-            borderWidth: 2,
-            width: "30%",
-            textAlign: "center",
-            fontWeight: "bold",
-            margin: 10,
-          }}
-        >
-          작성
-        </Text>
-      </Pressable>
-    </>
-  );
-};
-
-const TodoSearchScreen = () => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 40, fontWeight: "bold" }}>할일 검색</Text>
-    </View>
-  );
-};
-
-const TodoListScreen = () => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 40, fontWeight: "bold" }}>할일 리스트</Text>
-    </View>
-  );
-};
-
-const MyPageScreen = () => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 40, fontWeight: "bold" }}>내 정보</Text>
-    </View>
-  );
-};
+import tabConfig from "./configs/tabConfig";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const tabConfig = [
-    {
-      name: "Home",
-      title: "메인 홈",
-      component: HomeScreen,
-      focusedIcon: "home-variant",
-      unfocuedIcon: "home-variant-outline",
-      iconComponent: MaterialCommunityIcons,
-    },
-    {
-      name: "TodoSearch",
-      title: "할일 검색",
-      component: TodoSearchScreen,
-      focusedIcon: "search-sharp",
-      unfocuedIcon: "search-outline",
-      iconComponent: Ionicons,
-    },
-    {
-      name: "TodoWrite",
-      title: "할일 작성",
-      component: TodoWriteScreen,
-      focusedIcon: "application-edit",
-      unfocuedIcon: "application-edit-outline",
-      iconComponent: MaterialCommunityIcons,
-    },
-    {
-      name: "TodoList",
-      title: "할일 리스트",
-      component: TodoListScreen,
-      focusedIcon: "list-sharp",
-      unfocuedIcon: "list-outline",
-      iconComponent: Ionicons,
-    },
-    {
-      name: "MyPage",
-      title: "내 정보",
-      component: MyPageScreen,
-      focusedIcon: "person-circle-sharp",
-      unfocuedIcon: "person-circle-outline",
-      iconComponent: Ionicons,
-    },
-  ];
-
   const screenOptions = ({ route }) => ({
     tabBarIcon: ({ focused, color, size }) => {
       const routeConfig = tabConfig.find(
