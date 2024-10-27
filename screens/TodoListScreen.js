98cb304e58ecae1fcd04d8c54c83a37967cdbad3
SangWon7242 <gotkdqja@gmail.com>
2024-10-27 14:24:15 +0900

    useTodosState를 각 스크린의 파라미터 값으로 넘김

--- screens/TodoListScreen.js
+++ screens/TodoListScreen.js
@@ -1,10 +1,13 @@
 import { Text, View } from "react-native";
 import React from "react";
 
-const TodoListScreen = () => {
+const TodoListScreen = ({ route }) => {
+  const { todos } = route.params?.todosState || { todos: [] };
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text style={{ fontSize: 40, fontWeight: "bold" }}>할일 리스트</Text>
+      <Text>{JSON.stringify(todos)}</Text>
     </View>
   );
 };
