7b85f873c4f7e5ecb746f590ac2ac1713444a1ee
SangWon7242 <gotkdqja@gmail.com>
2024-11-03 14:11:55 +0900

    스플래시 스크린 이미지 반영

--- App.js
+++ App.js
@@ -11,9 +11,6 @@ import * as Font from "expo-font";
 
 const { width, height } = Dimensions.get("window");
 
-// 스플래시 스크린이 자동으로 숨겨지지 않도록 설정
-SplashScreen.preventAutoHideAsync();
-
 const fetchFonts = () => {
   return Font.loadAsync({
     "gmarketsans-font": require("./assets/fonts/GmarketSansTTFMedium.ttf"),
@@ -43,13 +40,17 @@ export default function App() {
     const loadFonts = async () => {
       try {
         await fetchFonts(); // 폰트 로드
+        await new Promise((resolve) => setTimeout(resolve, 2000));
       } catch (e) {
         console.warn(e); // 폰트 로드 중 오류 발생시 경고
       } finally {
         setFontLoaded(true);
-        SplashScreen.hideAsync(); // 폰트 로드가 완료되면 스플래시 스크린을 숨겨줌
+        await SplashScreen.hideAsync(); // 폰트 로드가 완료되면 스플래시 스크린을 숨겨줌
       }
     };
+
+    // 스플래시 스크린이 자동으로 숨겨지지 않도록 설정
+    SplashScreen.preventAutoHideAsync();
 
     loadFonts();
   }, []);
